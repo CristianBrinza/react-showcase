f65ab6d97d7df861c096b72cc0e1246aeb3cce08
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-10-16 15:33:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Tabulation from './showcase/tabulation/Tabulation';
 import Home from "./pages/Home";
+import DragAndDrop from "./showcase/drag-and-drop/DragAndDrop";
 
 function App() {
   return (
       <Router>
         <Routes>
             <Route path="/"  element={<Home/>}/>
-          <Route path="/tabulation" element={<Tabulation />} />
+            <Route path="/tabulation" element={<Tabulation />} />
+            <Route path="/drag-and-drop" element={<DragAndDrop />} />
         </Routes>
       </Router>
   );
