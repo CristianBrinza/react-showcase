75bc71cbea643fbb93c74fbb53bdf64d0765de7a
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-10-16 16:10:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+// src/App.tsx
+
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 import Tabulation from './showcase/tabulation/Tabulation';
-import Home from "./pages/Home";
-import DragAndDrop from "./showcase/drag-and-drop/DragAndDrop";
+import DragAndDrop from './showcase/drag-and-drop/DragAndDrop';
+import pagesData from './json/pages.json';
+
+interface Page {
+    path: string;
+    component: string;
+}
 
 function App() {
-  return (
-      <Router>
-        <Routes>
-            <Route path="/"  element={<Home/>}/>
-            <Route path="/tabulation" element={<Tabulation />} />
-            <Route path="/drag-and-drop" element={<DragAndDrop />} />
-        </Routes>
-      </Router>
-  );
+    const [pages, setPages] = useState<Page[]>([]);
+
+    useEffect(() => {
+        setPages(pagesData);
+    }, []);
+
+    const getComponent = (componentName: string) => {
+        switch (componentName) {
+            case 'Tabulation':
+                return <Tabulation />;
+            case 'DragAndDrop':
+                return <DragAndDrop />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {pages.map((page) => (
+                    <Route key={page.path} path={page.path} element={getComponent(page.component)} />
+                ))}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
